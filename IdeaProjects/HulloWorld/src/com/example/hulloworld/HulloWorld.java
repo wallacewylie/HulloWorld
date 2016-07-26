ad364e1662a43775850301ffdded19e37ea7e69a
wallacewylie <wallace.wylie@gmail.com>
2016-07-26 12:56:00 -0500

    Initial Commit

---
+++ IdeaProjects/HulloWorld/src/com/example/hulloworld/HulloWorld.java
@@ -0,0 +1,10 @@
+package com.example.hulloworld;
+
+/**
+ * Created by wallacewylie on 7/26/16.
+ */
+public class HulloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hullo, World!");
+    }
+}
