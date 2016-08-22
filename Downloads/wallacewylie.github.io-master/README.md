f6024cc3e2a405f627b8b994a594201eed381a0c
wallacewylie <wallace.wylie@gmail.com>
2016-08-21 22:47:07 -0500

    start of new website

---
+++ Downloads/wallacewylie.github.io-master/README.md
@@ -0,0 +1,4 @@
+# wallacewylie.github.io
+My Resume
+
+An online resume created using HTML and CSS.
---
+++ IdeaProjects/JavaIntellijTest/src/JavaIntellijTest.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by wallacewylie on 7/26/16.
+ */
+public class JavaIntellijTest {
+}
